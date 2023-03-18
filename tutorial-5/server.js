401cb7ba8045c93fb6fd7ff71fcf4eea3068f8ef
Mekael Turner <mistnpeace@gmail.com>
2023-03-18 18:54:06 +0800

    Serve error returns path.parse object

--- tutorial-5/server.js
+++ tutorial-5/server.js
@@ -18,20 +18,53 @@ const PORT = process.env.PORT || 3500;
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
-  // not optimal method to serve a page
-  let filePath;
-  // using a switch statement to serve a route
-  // it would be very big and you'd have to think of every file
-  // not dynamic
-  switch (req.url) {
-    case '/':
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html');
-      filePath = path.join(__dirname, 'views', 'index.html');
-      fs.readFile(filePath, 'utf8', (err, data) => {
-        res.end(data);
-      });
+  const extension = path.extname(req.url);
+  let contentType;
+
+  switch (extension) {
+    case '.css':
+      contentType = 'text/css';
       break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.jpg':
+      contentType = 'image/jpeg';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.txt':
+      contentType = 'text/plain';
+      break;
+    default:
+      contentType = 'text/html';
+  }
+
+  // Chain ternary statements
+  let filePath =
+    contentType === 'text/html' && req.url === '/'
+      ? path.join(__dirname, 'views', 'index.html')
+      : contentType === 'text/html' && req.url.slice(-1) === '/'
+      ? path.join(__dirname, 'views', req.url, 'index.html')
+      : contentType === 'text/html'
+      ? path.join(__dirname, 'views', req.url)
+      : path.join(__dirname, req.url);
+
+  // Makes .html extension not required in the browser
+  if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+  const fileExists = fs.existsSync(filePath);
+
+  if (fileExists) {
+    //serve the file
+  } else {
+    // 404
+    // 301 redirect
+    console.log(path.parse(filePath));
   }
 });
 
