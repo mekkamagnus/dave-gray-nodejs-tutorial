c986f526914d869814dd15c2934333d4141c14d6
Mekael Turner <mistnpeace@gmail.com>
2023-03-18 18:10:30 +0800

    serve html version 1

--- tutorial-5/server.js
+++ tutorial-5/server.js
@@ -16,6 +16,18 @@ const PORT = process.env.PORT || 3500;
 // Create a minimal server
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+  // not optimal method to serve a page
+  let filePath;
+  // a route to the root path
+  if (req.url === '/' || req.url === 'index.html') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    filePath = path.join(__dirname, 'views', 'index.html');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      res.end(data);
+    });
+  }
 });
 
 // Listen for request
